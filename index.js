82448461fbf3825d471a5b2701b8199090bcd581
Surya Sharma <suryasharma73077@gmail.com>
2025-08-20 11:20:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ const url = "http://numbersapi.com/random/math?json";
 const factpara = document.querySelector(".board");
 const btn = document.querySelector("#btn");
 const conclu = document.querySelector("#conclusion");
-
 let getfacts = async () => {
   console.log("Getting data...");
   let response = await fetch(url);
-  factpara.innerText = response;
+  let data = await response.json();
+  factpara.innerText = data.text;
 
   setTimeout(() => {
     conclu.innerText = "Click on above button";
-  }, 2000);
+  }, 500);
 };
-
-
 btn.addEventListener("click", getfacts);
-
